3b99050f271255c795a99cb9afb8db3e9d667858
Mark Hegedus <mark.hegedus@opensignal.com>
2024-10-07 13:49:47 +0200

    debug

--- client.go
+++ client.go
@@ -87,7 +87,7 @@ func (c *alertaClient) createKey(ctx context.Context, user string, scopes []stri
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusCreated {
-		return nil, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+		return nil, fmt.Errorf("unexpected status code: %d, %s", resp.StatusCode, jsonBody)
 	}
 
 	body, err := io.ReadAll(resp.Body)
